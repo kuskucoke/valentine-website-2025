18509b83195c67cd273f51a3754324dd08c421b1
kuskucoke <kuskucoke@gmail.com>
2026-02-13 19:56:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ const CONFIG = {
         musicUrl: "https://res.cloudinary.com/dbxuwafzj/video/upload/v1770728844/Fanny_Soegi_-_Jogja_Lantai_Dua_Official_Lyric_Video_ylyha5.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 0.3                        // Volume level (0.0 to 1.0)
     }
 };
 
